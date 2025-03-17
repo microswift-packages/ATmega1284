c1f03c72ddb4e6c8a16e363eb237ab87513d61ae
Carl Peto <CPeto@becrypt.com>
2025-03-17 23:31:03 +0000

    package fix

--- Package.swift
+++ Package.swift
@@ -9,7 +9,9 @@ let package = Package(
             name: "ATmega1284",
             targets: ["ATmega1284"]),
     ],
-    dependencies: [.package(path: "/Users/carl/Documents/Code/HALGEN/build/ATmega1284//../HAL")],
+    dependencies: [
+    .package(url: "https://github.com/microswift-packages/hal-baseline", from: "2.0.0")
+    ],
     targets: [
         .target(
             name: "ATmega1284",
